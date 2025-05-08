486bd07b2558efef21230d057400dda84f544021
yhy <2386358864yhy@gmail.com>
2025-05-08 22:15:58 +0800

    step6

---
+++ step6/_.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string c;
+	cin >> c;
+	cout << "(a)add account (d)deposit (w)withdraw (s)show (c)change day (n)next month (q)query (e)exit" << endl;
+	cout << "2008 - 11 - 1       Total: 0        command >" << endl;
+}
+
+
+
